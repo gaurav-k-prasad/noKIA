03fea0e47fdb41320bf32170d9774230edb43b75
Subaru <grdshivam89@gmail.com>
2026-02-14 18:46:26 +0530

    central side esp code

---
+++ nodes/central_esp.ino
@@ -0,0 +1,55 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+#define SS 5
+#define RST 14
+#define DIO0 26
+#define LORA_FREQ 433E6
+
+bool transmitMode = false;
+
+void setup() {
+  Serial.begin(115200);
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(LORA_FREQ)) {
+    while (1);
+  }
+
+  LoRa.setSpreadingFactor(7);
+  LoRa.setSignalBandwidth(250E3);
+  LoRa.setCodingRate4(5);
+  LoRa.enableCrc();
+  LoRa.setTxPower(20);
+
+  Serial.println("LoRa TEXT MODE READY");
+}
+
+void loop() {
+
+  // -------- SERIAL → LORA --------
+  if (Serial.available()) {
+
+    String msg = Serial.readStringUntil('\n');
+
+    LoRa.beginPacket();
+    LoRa.print(msg);
+    LoRa.endPacket();
+
+    Serial.println("SENT");
+  }
+
+  // -------- LORA → SERIAL --------
+  int packetSize = LoRa.parsePacket();
+
+  if (packetSize) {
+
+    String received = "";
+
+    while (LoRa.available()) {
+      received += (char)LoRa.read();
+    }
+
+    Serial.println("RX:" + received);
+  }
+}
